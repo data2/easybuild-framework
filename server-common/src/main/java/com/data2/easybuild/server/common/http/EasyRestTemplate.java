0ec9eca99686b8cae6444f0ffe0ad3cdd121f4fd
data2 <muskteerathos@gmail.com>
2021-01-28 17:18:25 +0800

    框架增加http调用方法、连接池以及配置

--- server-common/src/main/java/com/data2/easybuild/server/common/http/EasyRestTemplate.java
+++ server-common/src/main/java/com/data2/easybuild/server/common/http/EasyRestTemplate.java
@@ -1,12 +1,19 @@
 package com.data2.easybuild.server.common.http;
 
+import com.data2.easybuild.api.common.utils.ParamUtil;
+import lombok.Data;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-import sun.net.www.http.HttpClient;
+
+import javax.annotation.PostConstruct;
 
 /**
  * @author data2
@@ -14,10 +21,52 @@ import sun.net.www.http.HttpClient;
  * @date 2021/1/28 上午11:25
  */
 @Component
+@Configuration
+@ConfigurationProperties(prefix = "easy.http")
+@Data
 public class EasyRestTemplate extends RestTemplate {
 
-    public EasyRestTemplate(ClientHttpRequestFactory clientHttpRequestFactory){
-        super(clientHttpRequestFactory);
+    private Integer connectionRequestTimeout;
+    private Integer connectTimeout;
+    private Integer socketTimeout;
+
+    private Integer maxTotal;
+    private Integer defaultMaxPerRoute;
+
+    @PostConstruct
+    public void easyRestTemplate() {
+        this.setRequestFactory(clientHttpRequestFactory());
     }
+
+    /**
+     * client connect strategy
+     *
+     * @return
+     */
+    @Bean
+    public ClientHttpRequestFactory clientHttpRequestFactory() {
+        return new HttpComponentsClientHttpRequestFactory(httpClientBuilder().build());
+    }
+
+    @Bean
+    public PoolingHttpClientConnectionManager poolingHttpClientConnectionManager() {
+        PoolingHttpClientConnectionManager poolingHttpClientConnectionManager = new PoolingHttpClientConnectionManager();
+        poolingHttpClientConnectionManager.setMaxTotal(ParamUtil.isNull(connectTimeout, 100));
+        poolingHttpClientConnectionManager.setDefaultMaxPerRoute(ParamUtil.isNull(connectTimeout, 100));
+        return poolingHttpClientConnectionManager;
+    }
+
+    @Bean
+    public RequestConfig requestConfig() {
+        return RequestConfig.custom().setConnectionRequestTimeout(ParamUtil.isNull(connectionRequestTimeout, 30 * 1000))
+                .setConnectTimeout(ParamUtil.isNull(connectTimeout, 60 * 1000))
+                .setSocketTimeout(ParamUtil.isNull(socketTimeout, 60 * 1000)).build();
+    }
+
+    @Bean
+    public HttpClientBuilder httpClientBuilder() {
+        return HttpClientBuilder.create().setConnectionManager(poolingHttpClientConnectionManager()).setDefaultRequestConfig(requestConfig());
+    }
+
 
 }
