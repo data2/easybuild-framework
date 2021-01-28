ffce76cd354a8a305df21e18e0fbdab749e17662
data2 <muskteerathos@gmail.com>
2021-01-28 17:16:09 +0800

    测试demo

--- example/src/main/java/com/data2/easybuild/example/ExampleApplication.java
+++ example/src/main/java/com/data2/easybuild/example/ExampleApplication.java
@@ -1,7 +1,12 @@
 package com.data2.easybuild.example;
 
+import com.data2.easybuild.server.common.http.EasyRestTemplate;
+import com.data2.easybuild.server.common.lock.SingleRedisDistributeLock;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class ExampleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ExampleApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(ExampleApplication.class, args);
+        EasyRestTemplate rest = context.getBean(EasyRestTemplate.class);
+        RequestConfig requestConfigRequestConfig = context.getBean(RequestConfig.class);
+        PoolingHttpClientConnectionManager pool = context.getBean(PoolingHttpClientConnectionManager.class);
+        SingleRedisDistributeLock lock = context.getBean(SingleRedisDistributeLock.class);
+        System.out.println(rest.getConnectionRequestTimeout());
     }
 
     @GetMapping("/hi")
