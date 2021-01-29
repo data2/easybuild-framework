098d7db8821200f354a02450e08f96ccc7a13ad2
data2 <muskteerathos@gmail.com>
2021-01-29 20:11:33 +0800

    框架允许跨域请求

---
+++ server-common/src/main/java/com/data2/easybuild/server/common/security/CorsFilter.java
@@ -0,0 +1,35 @@
+package com.data2.easybuild.server.common.security;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author data2
+ * @description
+ * @date 2021/1/29 下午8:00
+ *
+ * all interface enable cors , use this
+ * if u want some interface, can use springboot anno @CrossOrigin
+ */
+@Configuration
+@ConditionalOnProperty(prefix = "easy.cors", name = "enable",havingValue = "true")
+public class CorsFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse res = (HttpServletResponse) response;
+        res.addHeader("Access-Control-Allow-Credentials", "true");
+        res.addHeader("Access-Control-Allow-Origin","*");
+        res.addHeader("Access-Control-Allow-Methods","GET, POST, DELETE, PUT");
+        res.addHeader("Access-Control-Allow-Headers","Content-Type,X-CAF-Authorization-Token,sessionToken,X-TOKEN");
+        if (((HttpServletRequest)request).getMethod() == "OPTIONS"){
+            res.getOutputStream().print("ok");
+            return;
+        }
+        chain.doFilter(request, response);
+    }
+}
