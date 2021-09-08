41deb8fccc2d67d9e1f2c35ae8b31d58c9594109
data2 <muskteerathos@gmail.com>
2021-09-08 10:53:23 +0800

    修改banner

---
+++ example/src/main/java/com/data2/easybuild/example/bean/OrderBean.java
@@ -0,0 +1,24 @@
+package com.data2.easybuild.example.bean;
+
+import com.data2.easybuild.api.common.rest.dto.AbstractRestRequest;
+import com.data2.easybuild.api.common.utils.ParamUtil;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author data2
+ * @description
+ * @date 2021/3/15 下午5:21
+ */
+@Data
+@NoArgsConstructor
+public class OrderBean extends AbstractRestRequest {
+    private String orderId;
+    private String action;
+
+    @Override
+    public void check() {
+        super.check();
+        ParamUtil.nullThrowException(orderId, "orderId不能为空");
+    }
+}
