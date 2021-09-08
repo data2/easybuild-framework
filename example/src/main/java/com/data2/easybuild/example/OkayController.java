82fc44c357f72cfcf57674a3bb7ae3801cdf6eff
data2 <muskteerathos@gmail.com>
2021-09-08 10:54:19 +0800

    测试

--- example/src/main/java/com/data2/easybuild/example/OkayController.java
+++ example/src/main/java/com/data2/easybuild/example/OkayController.java
@@ -1,7 +1,7 @@
 package com.data2.easybuild.example;
 
 import com.data2.easybuild.api.common.exception.EasyBusinessException;
-import com.data2.easybuild.example.bean.TestReq;
+import com.data2.easybuild.example.bean.OrderBean;
 import com.data2.easybuild.server.common.anno.DisableDuplicateSubmit;
 import com.data2.easybuild.server.common.dup.DupEnum;
 import com.data2.easybuild.server.common.security.EncryptRequest;
@@ -16,19 +16,39 @@ import org.springframework.web.bind.annotation.*;
  * @description
  * @date 2020/12/24 下午4:32
  */
+@RequestMapping("/okay")
 @RestController
 public class OkayController {
 
     @Autowired(required = false)
     private SnowflakeIdWorker snowflakeIdWorker;
 
-//    @DisableDuplicateSubmit(type = DupEnum.REQUEST_HASH, timeout = 2000)
-    @GetMapping("/test")
-//    @EncryptRequest
-    public Object test(){
-        System.out.println("comein ");
-        System.out.println(snowflakeIdWorker.nextId());
+
+    @GetMapping("/responseWrapper")
+    public Object ResponseWrapper(){
         return snowflakeIdWorker.nextId();
-        //throw new EasyBusinessException("837942");
+    }
+
+    @GetMapping("/void")
+    public Object void1(){
+        return true;
+    }
+
+    @GetMapping("/except")
+    public Object except(){
+        throw new EasyBusinessException("error");
+    }
+
+
+    @GetMapping("/testEncrypt")
+    @EncryptRequest
+    public Object testEncrypt(OrderBean orderBean){
+        return orderBean.getOrderId();
+    }
+
+    @GetMapping("/testDup")
+    @DisableDuplicateSubmit(type = DupEnum.REQUEST_HASH, timeout = 2000)
+    public Object testDup(OrderBean orderBean){
+        return orderBean.getOrderId();
     }
 }
