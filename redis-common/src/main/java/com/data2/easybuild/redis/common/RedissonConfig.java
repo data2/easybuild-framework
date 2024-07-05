f204a37fa6f100a31c52925b00529f21966614e0
data2 <muskteerathos@gmail.com>
2024-07-05 15:06:50 +0800

    param inject better

--- redis-common/src/main/java/com/data2/easybuild/redis/common/RedissonConfig.java
+++ redis-common/src/main/java/com/data2/easybuild/redis/common/RedissonConfig.java
@@ -1,18 +1,9 @@
 package com.data2.easybuild.redis.common;
 
 import lombok.Data;
-import org.redisson.Redisson;
-import org.redisson.api.RMap;
-import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import static com.data2.easybuild.redis.common.RedissonConfig.PREFIX;
-
 
 /**
  * @author data2
@@ -21,9 +12,6 @@ import static com.data2.easybuild.redis.common.RedissonConfig.PREFIX;
  */
 @Data
 @Configuration
-@ConfigurationProperties(prefix = PREFIX)
-public class RedissonConfig extends Config{
-
-    public static final String PREFIX = "easy.redis.redisson.config";
-
+@ConfigurationProperties(prefix = "easy.redis.redisson.config")
+public class RedissonConfig extends Config {
 }
--- redis-common/src/main/java/com/data2/easybuild/redis/common/RedissonConfiguration.java
+++ redis-common/src/main/java/com/data2/easybuild/redis/common/RedissonConfiguration.java
@@ -2,16 +2,9 @@ package com.data2.easybuild.redis.common;
 
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
-import org.redisson.config.Config;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 
 /**
  * @author data2
@@ -21,16 +14,9 @@ import org.springframework.stereotype.Component;
 @Configuration
 public class RedissonConfiguration {
 
-    private static final String TEST_STR = "1111";
-
-    @Autowired
-    private RedissonConfig redissonConfig;
-
     @Bean
     @ConditionalOnClass(RedissonConfig.class)
-    public RedissonClient redissonClient(){
-        RedissonClient client = Redisson.create(redissonConfig);
-        client.getAtomicLong(TEST_STR);
-        return client;
+    public RedissonClient redissonClient(RedissonConfig redissonConfig){
+        return Redisson.create(redissonConfig);
     }
 }
